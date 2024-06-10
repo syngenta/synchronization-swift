1584d942c6d7ef28f7fc23f83b5fde8609340073
Yevhenii Kalashnikov <lumyk@me.com>
2024-06-10 17:39:16 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,30 +1,26 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "Synchronization-swift",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Synchronization-swift",
-            targets: ["Synchronization-swift"]),
+            targets: ["Synchronization-swift"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.13.0")
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "8.1.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Synchronization-swift",
-            dependencies: ["PromiseKit"]
-        ),
-//        .testTarget(
-//            name: "Synchronization-swiftTests",
-//            dependencies: ["Synchronization-swift"]),
-    ]
+            dependencies: ["PromiseKit"],
+            path: "Sources"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
